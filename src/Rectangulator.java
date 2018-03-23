c35dc64e56c2a05d64101138f85fbf8d006e91d4
fporres <fernando.porres@tecnilogica.com>
2018-03-23 09:14:32 +0100

    Fix everything. First Java program compiling

--- src/Rectangulator.java
+++ src/Rectangulator.java
@@ -1,15 +1,29 @@
 public class Rectangulator {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         int length = Integer.parseInt(args[0]);
         int width  = Integer.parseInt(args[1]);
 
         Rectangle myRectangle = new Rectangle(length, width);
 
-        String output = String.format("*** Your Rectangle ***\n\nLength:    %d\nWidth:     %d\nArea:      %d\nPerimeter: %d\nmyRectangle.getLength(),myRectangle.getWidth(),myRectangle.getArea(),myRectangle.getPerimeter()");
+        StringBuilder sb = new StringBuilder();
+        sb.append("*** Your Rectangle ***");
+        sb.append("\n\n");
+        sb.append("Length:    %d");
+        sb.append("\n");
+        sb.append("Width:     %d");
+        sb.append("\n");
+        sb.append("Area:      %d");
+        sb.append("\n");
+        sb.append("Perimeter: %d");
+        sb.append("\n");
 
-        System.out.println(output);
+        System.out.println(String.format(sb.toString(),
+            myRectangle.getLength(),
+            myRectangle.getWidth(),
+            myRectangle.getArea(),
+            myRectangle.getPerimeter()));
     }
 
 }
